adb809235c44e0e0254fb054fe14875da0258ffa
abdoudevprog <abdoudevprog@gmail.com>
2026-07-31 10:47:10 +0100

    feat: remove old admin controllers and add new freeze module files

--- src/User/Client/transfert/src/TransfertControllers.js
+++ src/User/Client/transfert/src/TransfertControllers.js
@@ -1,14 +1,6 @@
 import * as clientService from './clientService.js'; 
 
-// export const deposit = async (req, res) => {
-//     try {
-          
-//         const depositeOpt = await clientService.deposit(req.body); 
-//         res.status(200).json(depositeOpt);
-//     } catch (error) {
-//         res.status(error.status || 500).json({ error: error.message });
-//     }
-// };
+
 
 export const requestTransfer = async (req, res) => {
     try {
